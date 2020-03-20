33f4223e67b6e2b971c0db51b78d163ea5c51f31
Zer0- <Zer000.cloudbyte@gmail.com>
2020-03-20 01:49:03 -0400

    login

--- src/Main.hs
+++ src/Main.hs
@@ -9,17 +9,22 @@ import qualified Data.ByteString.Base64.URL as BSURL
 import Data.Semigroup (Endo (..))
 import Control.Monad.IO.Class (liftIO)
 import qualified Crypto.Hash.MD5 as MD5
-import Network.HTTP.Client (defaultRequest)
+import Network.HTTP.Client (defaultRequest, CookieJar)
 import Network.HTTP.Types (renderQuery, queryTextToQuery)
 import Network.HTTP.Req
     ( runReq
     , req
     , defaultHttpConfig
     , GET (..)
+    , POST (..)
     , NoReqBody (..)
+    , ReqBodyUrlEnc (..)
+    , FormUrlEncodedParam
     , https
     , bsResponse
+    , ignoreResponse
     , responseBody
+    , responseCookieJar
     , (=:) --query params
     , (/:)
     , Option (Option)
@@ -36,12 +41,19 @@ import FSMemoize (fsmemoize)
 old_booru_base_url :: Url 'Https
 old_booru_base_url = https "lefty.booru.org" /: "index.php"
 
+new_booru_base_url :: Url 'Https
+new_booru_base_url = https "leftypol.booru.org" /: "index.php"
+
 old_booru_base_params :: Option 'Https
 old_booru_base_params
-    = "page" =: ("post" :: String)
+    =  "page" =: ("post" :: String)
     <> "s" =: ("list" :: String)
     <> "tags" =: ("all" :: String)
---    <> "pid" =: 0
+
+new_booru_login_params :: Option 'Https
+new_booru_login_params
+    =  "page" =: ("login" :: String)
+    <> "code" =: ("00" :: String)
 
 getRawPageBody_ :: Url scheme -> Option scheme -> IO ByteString
 getRawPageBody_ url params = runReq defaultHttpConfig $ do
@@ -101,15 +113,48 @@ fetchOldBooruImagePage datadir params = do
     mapM_ (putStrLn . ((++) "  ")) tags
     putStrLn ""
 
-main :: IO ()
-main = do
+-- parse data file
+-- leftybooru login?
+login :: Url scheme -> Option scheme -> FormUrlEncodedParam -> IO CookieJar
+login url params payload = runReq defaultHttpConfig $
+    (req
+        POST
+        url
+        (ReqBodyUrlEnc payload)
+        ignoreResponse
+        params
+    )
+    >>= liftIO . return . responseCookieJar
+
+mkloginParams :: String -> String -> FormUrlEncodedParam
+mkloginParams username password
+    =  "user" =: username
+    <> "pass" =: password
+    <> "submit" =: ("Log+in" :: String)
+
+
+archiveOldMetadata :: IO ()
+archiveOldMetadata = do
     args <- getArgs
     let datadir = head args
 
     let img_links =
             (mapM (fetchOldBooruPage datadir) idlist) :: IO [[ Option 'Https ]]
     
-    img_links >>= ((mapM_ (fetchOldBooruImagePage datadir)) . concat)
+    img_links >>= (mapM_ (fetchOldBooruImagePage datadir)) . concat
 
     where
         idlist = [i * 20 | i <- [0..(10800 `quot` 20)]]
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let username = head args
+    let password = head $ drop 1 args
+
+    cookies <- login
+        new_booru_base_url
+        new_booru_login_params
+        (mkloginParams username password)
+
+    print cookies
